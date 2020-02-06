d3d42e197273dbcc835c4b762aa47436a215543e
Shota <shotanozadze8@gmail.com>
2020-02-07 02:13:35 +0400

    upd

---
+++ .ipynb_checkpoints/DataParser-checkpoint.py
@@ -0,0 +1,58 @@
+#####################################
+###### STATIC FINAL VARIABLES #######
+#####################################
+PATH_TO_DATA_FOLDER = "./Data"
+DATA_SET_GROUP = ["Train", "Test"]
+#####################################
+
+import glob
+import random
+
+
+class DataParser:
+
+    def __init__(self):
+        self.data_pathes = self.all_wav_files_path()
+
+    def all_wav_files_path(self, path_to_data_folder = PATH_TO_DATA_FOLDER):
+        data_pathes = dict()
+
+        for data_set_group in DATA_SET_GROUP:
+            files_path = PATH_TO_DATA_FOLDER + "/" + data_set_group
+            set_group_data = dict()
+            for i in range(5):
+                files_path_with_format = files_path + "/" + str(i+1) + "/*.wav"
+                files_in_the_path = glob.glob(files_path_with_format)
+                set_group_data[i+1] = files_in_the_path
+            data_pathes[data_set_group] = set_group_data
+
+        return data_pathes
+
+    def return_one_random_path_file(self, isTrain: bool, number_voice: int):
+        random_file_index = random.randint(0, self.length_of_files_in_folder(isTrain,number_voice)-1)
+        return self.data_pathes[DATA_SET_GROUP[0]][number_voice][random_file_index] if isTrain else self.data_pathes[DATA_SET_GROUP[1]][number_voice][random_file_index]
+
+    def return_data_pathes(self):
+        return self.data_pathes
+
+    def length_of_files_in_folder(self, isTrain: bool, number_voice: int):
+        return len(self.data_pathes[DATA_SET_GROUP[0]][number_voice]) if isTrain else len(self.data_pathes[DATA_SET_GROUP[1]][number_voice])
+
+    def return_data_path_on_coordinates(self, isTrain: bool, number_voice: int, index_in_data: int):
+        return self.data_pathes[DATA_SET_GROUP[0]][number_voice][index_in_data] if isTrain else self.data_pathes[DATA_SET_GROUP[1]][number_voice][index_in_data]
+
+    def parse_file_name_to_creator_name(self, file_path: str):
+        splitted_path = file_path.split("/")
+        file_name = splitted_path[len(splitted_path)-1]
+        first_name, last_name, _ = file_name.split("-")
+        return first_name,last_name
+        
+
+if __name__ == "__main__":
+    data_parser = DataParser()
+    #print(data_parser.return_one_random_path_file(True, 1))
+    #print(data_parser.return_data_pathes())
+    file_path = data_parser.return_data_path_on_coordinates(True, 1, 2)
+    print("parsing file on path:",file_path)
+    first_name, last_name = data_parser.parse_file_name_to_creator_name(file_path)
+    print(first_name, last_name, "is this file creator")
---
+++ .ipynb_checkpoints/DataProvider-checkpoint.py
@@ -0,0 +1,80 @@
+import numpy as np
+import DataParser
+import VAD
+import random
+import librosa
+from scipy import signal
+
+class DataProvider:
+
+    def __init__(self, isTrain = True):
+        self.isTrain = isTrain
+        self.dataParser = DataParser.DataParser()
+        self.vad = VAD.VAD()
+        self.dataPathDictionary = self.__createDicitonaryForDataPath__()
+        # while self.hasNext():
+        #     spectrogram, number_list = self.next()
+        #     print(number_list, ') ', spectrogram.shape)
+        #print(self.dataParser.__return_data_pathes__())
+
+
+    def hasNext(self):
+        return True if len(self.dataPathDictionary.keys()) else False
+
+    def next(self):
+        dataPathDictionaryKeyLength = len(self.dataPathDictionary.keys())
+        randomNumber = list(self.dataPathDictionary.keys())[random.randint(0,dataPathDictionaryKeyLength-1)]
+        randomDataPathIndexInList = random.randint(0, len(self.dataPathDictionary[randomNumber])-1)
+        randomDataPathIndexInDataParser = self.dataPathDictionary[randomNumber][randomDataPathIndexInList]
+        del self.dataPathDictionary[randomNumber][randomDataPathIndexInList]
+
+        if len(self.dataPathDictionary[randomNumber]) == 0:
+            del self.dataPathDictionary[randomNumber]
+        
+        randomDataPath = self.dataParser.return_data_path_on_coordinates(self.isTrain, randomNumber, randomDataPathIndexInDataParser)
+        
+        result = np.zeros(5)
+        if self.isTrain:
+            result[randomNumber - 1] = 1
+        else:
+            randomNumber = self.dataParser.parse_file_name_to_number(randomDataPath)
+            result[randomNumber - 1] = 1
+        return self.__getDataFromPath__(randomDataPath), result
+
+    
+    def __createDicitonaryForDataPath__(self):
+        dataPathDictionary = dict()
+        if self.isTrain:
+            for number in range(1,6,1):
+                dataCountOfNumber = self.dataParser.length_of_files_in_folder(self.isTrain, number)
+                dataPathDictionary[number] = list(range(dataCountOfNumber))
+            return dataPathDictionary
+        else:
+            dataCountOfNumber = self.dataParser.length_of_files_in_folder(self.isTrain, 0)
+            dataPathDictionary[0] = list(range(dataCountOfNumber))
+            return dataPathDictionary
+
+
+    def __getDataFromPath__(self, dataPath):
+        data_numpy, sampling_rate = librosa.load(dataPath, sr=16000)
+        data_numpy_no_silence = self.vad.remove_silences(data_numpy, 0.01)
+        freqs, times, spectrogram = self.__log_specgram__(data_numpy_no_silence, sampling_rate)
+        mean = np.mean(spectrogram, axis=0)
+        std = np.std(spectrogram, axis=0)
+        spectrogram = (spectrogram - mean) / std
+        return spectrogram
+
+
+    def __log_specgram__(self, audio, sample_rate, window_size=20, step_size=10, eps=1e-10):
+        nperseg = int(round(window_size * sample_rate / 1e3))
+        noverlap = int(round(step_size * sample_rate / 1e3))
+        freqs, times, spec = signal.spectrogram(audio, fs=sample_rate, window='hann', nperseg=nperseg, noverlap=noverlap, detrend=False)
+        return freqs, times, np.log(spec.T.astype(np.float32) + eps)
+
+
+
+if __name__ == "__main__":
+    dataProvider = DataProvider(False)
+    while dataProvider.hasNext():
+        spectrogram, result = dataProvider.next()
+        print(spectrogram.shape, result)
